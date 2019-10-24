0e0d957d52c720203f05c1a9e698ba6faa568ee9
Dzulfikar Maulana <dzulfikar.maulana@brainworx.co.id>
2019-10-24 10:46:25 +0700

    03 Restful Web Services with Spring Boot: 016 Step 05 - Quick Review of Spring Boot Auto Configuration and Dispatcher Servlet

--- restful-web-services/src/main/java/com/gekif/rest/webservices/restfulwebservices/HelloWorldController.java
+++ restful-web-services/src/main/java/com/gekif/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.gekif.rest.webservices.restfulwebservices;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 // Controller
@@ -15,5 +16,11 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world-bean")
     public HelloWorldBean helloWorldBean() {
         return new HelloWorldBean("Hello World");
+    }
+
+    // /hello-world/path-variable/fikarGanteng
+    @GetMapping(path = "/hello-world/path-variable/{name}")
+    public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+        return new HelloWorldBean(String.format("Hello World, %s", name));
     }
 }
