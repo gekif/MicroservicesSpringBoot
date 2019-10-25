8c4f08a8d6114545c19bca6e2765c4f8c9bd879c
Dzulfikar Maulana <dzulfikar.maulana@brainworx.co.id>
2019-10-25 15:05:59 +0700

    03 Restful Web Services with Spring Boot: 038 Step 26 - Versioning RESTful Services - Basic Approach with URIs

---
+++ restful-web-services/src/main/java/com/gekif/rest/webservices/restfulwebservices/versioning/PersonV1.java
@@ -0,0 +1,21 @@
+package com.gekif.rest.webservices.restfulwebservices.versioning;
+
+public class PersonV1 {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public PersonV1(String name) {
+        this.name = name;
+    }
+
+    public PersonV1() {
+
+    }
+}
