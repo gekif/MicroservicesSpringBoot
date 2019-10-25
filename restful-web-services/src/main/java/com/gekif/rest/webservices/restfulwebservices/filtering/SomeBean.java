6fd02533bd6ce56182c356e48c39b5b3dfcf4e15
Dzulfikar Maulana <dzulfikar.maulana@brainworx.co.id>
2019-10-25 14:56:10 +0700

    03 Restful Web Services with Spring Boot: 037 Step 25 - Implementing Dynamic Filtering for RESTful Service

--- restful-web-services/src/main/java/com/gekif/rest/webservices/restfulwebservices/filtering/SomeBean.java
+++ restful-web-services/src/main/java/com/gekif/rest/webservices/restfulwebservices/filtering/SomeBean.java
@@ -1,9 +1,8 @@
 package com.gekif.rest.webservices.restfulwebservices.filtering;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonFilter;
 
-@JsonIgnoreProperties(value = {"field1", "field2"})
+@JsonFilter("SomeBeanFilter")
 public class SomeBean {
 
     private String field1;
