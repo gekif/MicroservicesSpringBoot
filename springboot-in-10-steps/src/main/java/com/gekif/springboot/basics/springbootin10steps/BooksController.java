351a1f5f3ec35ae5073bead3a87225f93ea0a64f
Dzulfikar Maulana <dzulfikar.maulana@brainworx.co.id>
2019-10-30 16:54:38 +0700

    06 Appendix - Introduction to Spring Boot in 10 Steps: 1111 Step 4  Creating a Simple REST Controller

---
+++ springboot-in-10-steps/src/main/java/com/gekif/springboot/basics/springbootin10steps/BooksController.java
@@ -0,0 +1,18 @@
+package com.gekif.springboot.basics.springbootin10steps;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class BooksController {
+
+    @GetMapping("/books")
+    public List<Book> getAllBooks() {
+        return Arrays.asList(
+                new Book(1l, "Kajian Kelayakan", "Dzulfikar Maulana"));
+    }
+}
+
