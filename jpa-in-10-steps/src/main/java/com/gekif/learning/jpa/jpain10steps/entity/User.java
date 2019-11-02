62a5a7c8cd49ec1d57112474c2f4f1bb0259d314
Dzulfikar Maulana <dzulfikar.maulana@brainworx.co.id>
2019-11-02 10:41:43 +0700

    07 Appendix - Introduction to JPA in 10 Steps: 123 Step 5  Defining a JPA Entity - User

---
+++ jpa-in-10-steps/src/main/java/com/gekif/learning/jpa/jpain10steps/entity/User.java
@@ -0,0 +1,61 @@
+package com.gekif.learning.jpa.jpain10steps.entity;
+
+// Table - User
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    private String name;
+
+    private String role;
+
+    public User(long id, String name, String role) {
+        this.id = id;
+        this.name = name;
+        this.role = role;
+    }
+
+    protected User() {
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", role='" + role + '\'' +
+                '}';
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
